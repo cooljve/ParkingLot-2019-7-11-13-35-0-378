a50ced2f7cdbf9a7ac6ae494475e368c9813224b
Joi Liu <547169735@qq.com>
2019-07-14 14:31:26 +0800

    finish story1's ac3

--- src/test/java/com/thoughtworks/tdd/ParkingSystemTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingSystemTest.java
@@ -10,35 +10,51 @@ import static org.junit.jupiter.api.Assertions.*;
 class ParkingSystemTest {
 
   @Test
-  public void should_return_car_when_park_car_fetch_car(){
+  public void should_return_car_when_park_car_fetch_car() {
     //give
-    Car car=new Car();
-    ParkingLot parkingLot=new ParkingLot();
-    ParkingBoy parkingBoy=new ParkingBoy();
+    Car car = new Car();
+    ParkingLot parkingLot = new ParkingLot();
+    ParkingBoy parkingBoy = new ParkingBoy();
     //when
     parkingBoy.setParkingLot(parkingLot);
-    ParkingTicket ticket=parkingBoy.park(car);
-    Car fetchedCar=parkingBoy.fetch(ticket);
+    ParkingTicket ticket = parkingBoy.park(car);
+    Car fetchedCar = parkingBoy.fetch(ticket);
     //then
     assertEquals(car, fetchedCar);
   }
 
   @Test
-  public void should_return_right_car_when_park_car_fetch_car(){
+  public void should_return_right_car_when_park_car_fetch_car() {
     //give
-    Car firstCar=new Car();
-    Car secondCar=new Car();
-    ParkingLot parkingLot=new ParkingLot();
-    ParkingBoy parkingBoy=new ParkingBoy();
+    Car firstCar = new Car();
+    Car secondCar = new Car();
+    ParkingLot parkingLot = new ParkingLot();
+    ParkingBoy parkingBoy = new ParkingBoy();
     //when
     parkingBoy.setParkingLot(parkingLot);
-    ParkingTicket ticket=parkingBoy.park(firstCar);
-    ParkingTicket ticket1=parkingBoy.park(secondCar);
-    Car fetchedCar=parkingBoy.fetch(ticket);
-    Car fetchedCar1=parkingBoy.fetch(ticket1);
+    ParkingTicket ticket = parkingBoy.park(firstCar);
+    ParkingTicket ticket1 = parkingBoy.park(secondCar);
+    Car fetchedCar = parkingBoy.fetch(ticket);
+    Car fetchedCar1 = parkingBoy.fetch(ticket1);
     //then
     assertEquals(firstCar, fetchedCar);
     assertEquals(secondCar, fetchedCar1);
+  }
+
+  @Test
+  public void should_null_when_fetch_car_with_wrong_ticket() {
+    //give
+    Customer customer = new Customer();
+    customer.setParkingTicket(new ParkingTicket());
+    ParkingLot parkingLot = new ParkingLot();
+    ParkingBoy parkingBoy = new ParkingBoy();
+    //when
+    parkingBoy.setParkingLot(parkingLot);
+    Car fetchedCar = parkingBoy.fetch(customer.getParkingTicket());
+    Car fetchedCar1 = parkingBoy.fetch(null);
+    //then
+    assertEquals(null, fetchedCar);
+    assertEquals(null, fetchedCar1);
   }
 
 }
